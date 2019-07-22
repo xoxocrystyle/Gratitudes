04b0bc8d3f20cb9c7e69236f14538408c15ed138
Crystal Shir Navarro <crystal.shir@gmail.com>
2019-07-21 22:39:02 -0700

    created post requests

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,14 +4,28 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 //get
-router.get('/', (req, res) => {
-    res.send('yo');
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
 });
 
 
 //add
-
+router.post('/', async(req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+})
 
 //delete
+async function loadPostsCollection(){
+    const client = await mongodb.MongoClient.connect('mongodb://cn123:abc123@ds253567.mlab.com:53567/gratitudes', {
+        useNewUrlParser: true
+    });
+    return client.db('gratitudes').collection('posts');
+}
 
 module.exports = router;
